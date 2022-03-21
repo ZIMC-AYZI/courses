29a01d49e21b2a6ae32f0a67a5b52899404e9931
Smalchenko <kiroobro2@gmail.com>
2022-03-21 21:07:14 +0200

    'hw1'

--- main.js
+++ main.js
@@ -4,30 +4,48 @@
 // //  если пользователь ввел некорректное значение вывести
 // //   сообщение "Такого дня недели не существует!"
 // РЕШЕНИЕ
+//
 // let result = +prompt('Напиши день недели (цифрой)');
 //
 // function week(day) {
-//     if (day === 1) {
-//         alert('Понедельник')
-//     } else if (day === 2) {
-//         alert('Вторник')
-//     } else if (day === 3) {
-//         alert('Среда')
-//     } else if (day === 4) {
-//         alert('Четверг')
-//     } else if (day === 5) {
-//         alert('Пятница')
-//     } else if (day === 6) {
-//         alert('Суббота')
-//     } else if (day === 7) {
-//         alert('Воскресенье')
-//     } else {
-//         alert('Такого дня недели не существует!')
+//     switch (day) {
+//         case 1: {
+//             alert('Понедельник');
+//             break;
+//         }
+//         case 2: {
+//             alert('Вторник');
+//             break;
+//         }
+//         case 3: {
+//             alert('Среда');
+//             break;
+//         }
+//         case 4: {
+//             alert('Четверг');
+//             break;
+//         }
+//         case 5: {
+//             alert('Пятница');
+//             break;
+//         }
+//         case 6: {
+//             alert('Суббота');
+//             break;
+//         }
+//         case 7: {
+//             alert('Воскресенье');
+//             break;
+//         }
+//         default: {
+//             alert('Такого дня недели не существует!')
+//         }
 //     }
 // }
 //
 // week(result);
 // TODO ПЕРЕПИСАТЬ НА ОПЕРАТОР SWITCH используя case
+// ГОТОВО
 
 
 // // 5. Задача (Условный оператор)
@@ -36,24 +54,18 @@
 //
 // РЕШЕНИЕ
 //
-// let first = +prompt('Давай сравним 2 числа, какое больше?');
-// let second = +prompt('Введи второе число');
-//
-// function math(number) {
-//     if (first < second) {
-//         alert(second)
-//     } else if (first > second) {
-//         alert(first)
-//     } else {
-//         alert('Числа равны')
-//     }
+// function math() {
+//     const first = +prompt('Давай сравним 2 числа, какое больше?');
+//     const second = +prompt('Введи второе число');
+//     alert(first > second ? first : first < second ? second : 'Числа равны');
 // }
+//
+// math();
 
 // TODO занеси промты (first,second) в функцию что-бы окна с вводом вылезали только при вызове функции
 // TODO нету смысла в функции принимать аргументы, т.к при ее вызове будет инициализированы константы а и б с которыми пойдет логика работы
 // TODO использую тернарный оператор исправь функцию на 1 строку EXAMPLE: const a = 1;const b = 2;console.log(a > b ? a : a < b ? b : 'числа равны')
-//
-// math(first);
+// ГОТОВО
 
 // // 6. Задача (Условный оператор)
 // // В первом подъезде квартиры с 1 по 20.
@@ -65,9 +77,8 @@
 //
 // РЕШЕНИЕ
 //
-// let entrance = +prompt('Введи номер квартиры, а я скажу в каком она подьезде');
-//
-// function result(number) {
+// function result() {
+//     let entrance = +prompt('Введи номер квартиры, а я скажу в каком она подьезде');
 //     if (entrance <= 20) {
 //         alert('Квартира в первом подьезде')
 //     } else if (entrance <= 48) {
@@ -79,8 +90,10 @@
 //     }
 // }
 //
+// result();
+
 // TODO вынести промты в функцию что-бы промт отрабатывал при вызове функции
-// result(entrance);
+// ГОТОВО
 
 // // 7. Задача (Условный оператор)
 // // Пользователь вводит логин и пароль.
@@ -91,20 +104,17 @@
 //
 // РЕШЕНИЕ
 //
-// let name = prompt('Введи логин');
-// let password = prompt('Введи пароль');
-//
-// function login(go) {
-//     if (name === 'Stas' && password === 'Eblan') {
-//         alert('Добро пожаловать!')
-//     } else {
-//         alert('Ошибка входа')
-//     }
+// function login() {
+//     let name = prompt('Введи логин');
+//     let password = prompt('Введи пароль');
+//     alert(name === 'Stas' && password === 'Huesos' ? 'Добро пожаловать' : 'Ошибка входа')
 // }
 //
+// login();
+
 // TODO вынести промты в функцию что-бы промт отрабатывал при вызове функции
 // TODO использую тернарный оператор исправь функцию на 1 строку EXAMPLE: const a = 1;const b = 2;console.log(a > b ? a : a < b ? b : 'числа равны')
-// login(name);
+// ГОТОВО
 
 // // 8. Задача (Условный оператор)
 // // Пользователь вводит год рождения. Программа показывает количество
@@ -117,15 +127,13 @@
 //     const birthday = prompt('Введите год рождения', '2020');
 //     const currentYear = new Date().getFullYear();
 //     const userAge = currentYear - birthday
-//     if (userAge >= 16) {
-//         alert('Добро пожаловать')
-//     } else {
-//         alert('Вход воспрещен')
-//     }
+//     alert(userAge >= 16 ? 'Добро пожаловать' : 'Вход воспрещен')
 // }
 //
-// TODO использую тернарный оператор исправь функцию на 1 строку EXAMPLE: const a = 1;const b = 2;console.log(a > b ? a : a < b ? b : 'числа равны')
 // age();
+//
+// TODO использую тернарный оператор исправь функцию на 1 строку EXAMPLE: const a = 1;const b = 2;console.log(a > b ? a : a < b ? b : 'числа равны')
+// ГОТОВО
 
 // // 9. Задача (Условный оператор)
 // // Создайте программу, которая выводит надбавку за стаж.
@@ -136,17 +144,14 @@
 //
 // function money() {
 //     const experience = prompt('Введите ваш стаж работы');
-//     if (experience <= 5) {
-//         alert('Ваша надбавка 50$')
-//     } else if (experience <= 10) {
-//         alert('Ваша надбавка 100$')
-//     } else {
-//         alert('Ваша надбавка 500$')
-//     }
+//     alert(experience <= 5 ? 'Ваша надбавка 50$' : experience <= 10 ? 'Ваша надбавка 100$' : 'Ваша надбавка 500$')
 // }
+// money();
 //
 // TODO использую тернарный оператор исправь функцию на 1 строку EXAMPLE: const a = 1;const b = 2;console.log(a > b ? a : a < b ? b : 'числа равны')
-// money();
+// ГОТОВО
+
+
 
 // //  10. Задача (креативная)
 // //  Написать программу которая будет склонять слово
@@ -216,4 +221,85 @@
 //
 // health();
 
-// 123
+// ЗАДАЧА С АЭРОПОРТОМ
+//
+// РЕШЕНИЕ
+//
+// function airport() {
+//     let airType = prompt('Введите тип самолета');
+//     const userTime = prompt('Введите ваше время вылета');
+//     const splitTime = userTime.split(':');
+//     const userTimeInMin = (+splitTime[0] * 60) + +splitTime[1];
+//     const aTerminal = {
+//         startTime: getMin(6),
+//         endTime: getMin(18),
+//         g1Start: getMin(6),
+//         g1End: getMin(10),
+//         g2Start: getMin(10),
+//         g2End: getMin(14),
+//         g3Start: getMin(14),
+//         g3End: getMin(18)
+//     };
+//     const bTerminal = {
+//         startTime: getMin(3),
+//         endTime: getMin(21),
+//         s10Start: getMin(3),
+//         s10End: getMin(12),
+//         s21Start: getMin(12),
+//         s21End: getMin(18),
+//         s32Start: getMin(20),
+//         s32End: getMin(21)
+//     };
+//     const cTerminal = {
+//         time: new Date(),
+//         n43Start: getMin(6),
+//         n43End: getMin(14),
+//         n35Start: getMin(14),
+//         n35End: getMin(20),
+//         n67Start: getMin(20),
+//         n67End: getMin(6)
+//     };
+//     if (airType === 'UK20B' && userTimeInMin >= aTerminal.g1Start && userTimeInMin <= aTerminal.g1End) {
+//         alert('Вам необходимо пройти в терминал А (ворота G1)')
+//     } else if (airType === 'UK20B' && userTimeInMin >= aTerminal.g2Start && userTimeInMin <= aTerminal.g2End) {
+//         alert('Вам необходимо пройти в терминал А (ворота G2)')
+//     } else if (airType === 'UK20B' && userTimeInMin >= aTerminal.g3Start && userTimeInMin <= aTerminal.g3End) {
+//         alert('Вам необходимо пройти в терминал А (ворота G3)')
+//     } else if (airType === 'GL8000' && userTimeInMin >= bTerminal.s10Start && userTimeInMin <= bTerminal.s10End) {
+//         alert('Вам необходимо пройти в терминал В (ворота S10)')
+//     } else if (airType === 'GL8000' && userTimeInMin >= bTerminal.s21Start && userTimeInMin <= bTerminal.s21End) {
+//         alert('Вам необходимо пройти в терминал В (ворота S21)')
+//     } else if (airType === 'GL8000' && userTimeInMin >= bTerminal.s32Start && userTimeInMin <= bTerminal.s32End) {
+//         alert('Вам необходимо пройти в терминал В (ворота S32)')
+//     } else if (airType === 'AX2022' && userTimeInMin >= aTerminal.g1Start && userTimeInMin <= aTerminal.g1End ||
+//         userTimeInMin >= cTerminal.n43Start && userTimeInMin <= cTerminal.n43End) {
+//         alert('Вы можете пройти в терминал А (ворота G1), либо в терминал C (ворота N43)')
+//     } else if (airType === 'AX2022' && userTimeInMin >= aTerminal.g2Start && userTimeInMin <= aTerminal.g2End ||
+//         userTimeInMin >= cTerminal.n43Start && userTimeInMin <= cTerminal.n43End) {
+//         alert('Вы можете пройти в терминал А (ворота G2), либо в терминал C (ворота N43)')
+//     } else if (airType === 'AX2022' && userTimeInMin >= aTerminal.g3Start && userTimeInMin <= aTerminal.g3End ||
+//         userTimeInMin >= cTerminal.n35Start && userTimeInMin <= cTerminal.n35End) {
+//         alert('Вы можете пройти в терминал А (ворота G3), либо в терминал C (ворота N35)')
+//     } else if (airType === 'AX2022' && userTimeInMin <= cTerminal.n67Start && userTimeInMin >= cTerminal.n67End) {
+//         alert('Вы можете пройти в терминал C (ворота N67)')
+//     } else {
+//         alert('Сосать плюс лежать')
+//     }
+// }
+//
+// function getMin(hours) {
+//     const date = new Date();
+//     date.setHours(hours);
+//     return date.getHours() * 60;
+// }
+//
+// airport();
+
+
+
+
+//
+const test1 = ['123', 'test', 2, 'dildo', 'maramoyka', 'salo', 'rysna ymerla', '99'];
+
+console.log(test1[test1.length -1]);
+console.log(test1[4]);
