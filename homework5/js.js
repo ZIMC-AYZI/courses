cb433c368104e588d7f90511e8ee739b09193b45
Smalchenko <kiroobro2@gmail.com>
2022-04-15 17:39:50 +0300

    'work4upd'

--- homework5/js.js
+++ homework5/js.js
@@ -7,6 +7,7 @@ let cards = [
         name: 'Lyuba',
         img: 'https://random.imagecdn.app/500/500',
         description: 'Masaj prostati',
+        id: 1
     }
 ];
 
@@ -19,7 +20,8 @@ function getRandomCard() {
     return {
         name: name[Math.floor(Math.random() * (name.length - 1)) + 1],
         img: getRandomImage(),
-        description: description[Math.floor(Math.random() * (description.length - 1)) + 1]
+        description: description[Math.floor(Math.random() * (description.length - 1)) + 1],
+        id: Math.floor(Math.random() * 1000)
     }
 }
 
@@ -27,16 +29,22 @@ function getRandomImage() {
     return `https://random.imagecdn.app/500/500`
 }
 
+function removeCard(id) {
+    cards = cards.filter(card => card.id !== id);
+    cardRender()
+}
+
 function cardRender() {
     let result = '';
 
     for (let i = 0; i < cards.length; i++) {
 
-        result += `<div class="card">
+        result += `<div class="card-wrapper"><div class="card">
             <p class="name">${cards[i].name}</p>
             <img class="img" src="${cards[i].img}">
             <p class="description">${cards[i].description}</p>
-        </div>`
+            <button onclick="removeCard(${cards[i].id})" class="btn-remove"></button>
+        </div></div>`
     }
 
     cardContainer.innerHTML = result;
