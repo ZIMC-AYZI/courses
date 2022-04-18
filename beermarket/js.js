46ad4b6a62f4d03c21bd3d4e0acb4a687a5ef07b
Smalchenko <kiroobro2@gmail.com>
2022-04-18 22:42:18 +0300

    'start'

---
+++ beermarket/js.js
@@ -0,0 +1,6 @@
+const searchBtn = document.querySelector('.search');
+const beerContainer = document.querySelector('.beer-container');
+
+searchBtn.addEventListener('click', () => {
+    console.log('search')
+});
