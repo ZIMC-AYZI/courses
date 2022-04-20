af4339974042bb7e2273c9410779ba3dc195a97c
Vogrant <vograntt@gmail.com>
2022-04-21 02:15:48 +0300

    was corrected 5task

--- homew5/homew-5.js
+++ homew5/homew-5.js
@@ -12,7 +12,7 @@ let cards = [
 function getRundom() {
     return {
         nameCard: nameArr[Math.floor(Math.random() * (nameArr.length-1)) + 1],
-        image: `https://random.imagecdn.app/500/500`,
+        image: getImg(),//`https://random.imagecdn.app/500/500`,
         descriptionName: descriptionArr[Math.floor(Math.random() * (descriptionArr.length - 1)) + 1],
         id: Math.floor(Math.random() * 1000)
     }
@@ -34,12 +34,18 @@ function deleteCards(id) {
     cardRender();
 }
 
+function getImg() {
+    const url = new URL(`https://random.imagecdn.app/500/500`)
+    return url
+
+}
+
 function cardRender() {
     let  res = '';
     for (let i = 0; i < cards.length; i++){
         res += `<div class="card">
-<p class="name-card">${cards[i].nameCard}</p>
 <img class="image" src="${cards[i].image}">
+<p class="name-card">${cards[i].nameCard}</p>
 <p class="description-name">${cards[i].descriptionName}</p>
 <button onclick="deleteCards(${cards[i].id})" class="btn-dele"></button>
 </div>`
