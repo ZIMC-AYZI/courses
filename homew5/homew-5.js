b168f6d5a61b40d4884fb62c4cadb05b64c9bd09
Vogrant <vograntt@gmail.com>
2022-04-25 13:19:54 +0300

    ready

--- homew5/homew-5.js
+++ homew5/homew-5.js
@@ -54,3 +54,41 @@ function cardRender() {
 }
 
 cardRender();
+
+// let bulbs =[
+//     {
+//         width: 90,
+//         height: 90,
+//         position: absolute,
+//         left: 364,
+//         top: 434
+//     }
+// ];
+//
+// function getRandomDiapason(min, max) {
+//     min = Math.ceil(min);
+//     max = Math.floor(max);
+//     return Math.floor(Math.random() * (max -min + 1)) + min;
+// }
+//
+// function getRandomBulbs() {
+//     const widthAndHeight = getRandomDiapason(10, 90);
+//     return {
+//         width: widthAndHeight,
+//         height: widthAndHeight,
+//         position: absolute,
+//         left: getRandomDiapason(20, 1220),
+//         top: getRandomDiapason(10, 450)
+//     }
+//
+// }
+//
+// function bulbRender() {
+//     let res = "";
+//     bulbs.length = 10;
+//     for (let i = 0; i < bulbs.length; i++ ){
+//         res += `<div class="bulb">
+// <img class="bulb1" src="${bulbs[i].}">
+// </div>>`
+//     }
+// }
