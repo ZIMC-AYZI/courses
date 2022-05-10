13824893d02ee39849bf3e15286230af41c4a301
Vogrant <vograntt@gmail.com>
2022-05-10 16:07:07 +0300

    mb

--- HW-6/homew5-2.js
+++ HW-6/homew5-2.js
@@ -1,20 +1,20 @@
 'use strict';
 let beerDat;
 let beerBasket = [];
-let page  = 1;
+let page = 1;
 let countBeer = 0;
 
 const cardsContainer = document.querySelector('.cards-container');
 const cardBasketContainer = document.querySelector('.card-basket-container');
 
 const myClick = () => {
+
     let input = document.querySelector('.input__value');
     if (input.value === ''){
         alert('ведите значение')
     }else {
         return input.value
     }
-
 };
 
 document.querySelector('.btn')
@@ -26,31 +26,31 @@ document.querySelector('.btn')
         });
 
 function showBasket (container) {
+
     const bask__btn = document.querySelector(container);
     bask__btn.style.display = 'block'
 }
-
+document.addEventListener('load', cardRenders ); //DOMContentLoaded, load
 document.addEventListener('scroll', scrollAdd);
 
 function scrollAdd () {
     let seeScroll = document.documentElement.clientHeight;
     let fullScroll = document.documentElement.scrollHeight;
-    let allPage = window.pageYOffset
+    let allPage = window.pageYOffset;
 
     let partOffSeeScroll = 6;
 
     let auxiliaryPart = Math.floor(seeScroll/partOffSeeScroll) ;
     let pointForNextPage =  Math.floor(fullScroll - seeScroll - auxiliaryPart) ;
-    let a = pointForNextPage + partOffSeeScroll
-    if (allPage >= pointForNextPage && allPage >= a) {
+    if (allPage >= pointForNextPage) {
         page++;
-        console.log(page)
+        console.log(page);
         getBeers();
     }
 
 }
 
-function getBeers () {
+function getBeers (isSelected = false) {
     //--------------------//\\ Это не удаляю как пример еще одного рабочего метода-----
     //--------------------------------    --------   --------   ------------   --------
     // const responseBeer = fetch(`https://api.punkapi.com/v2/beers?page=${page}&per_page=10&beer_name=${myClick()}`)
@@ -64,38 +64,33 @@ function getBeers () {
         fetch(`https://api.punkapi.com/v2/beers?page=${page}&per_page=10&beer_name=${myClick()}`)
             .then(response => response.json())
             .then(beerSort => {
-                if (page === 1){
-                    beerDat = [...beerSort];
-                }else {
-                    beerDat = [...beerSort,...beerDat];
-                }
+                beerDat = isSelected ? beerSort : [...beerDat,...beerSort];
             cardRenders();
-
         })
-    }
+}
 
 function cardRenders () {
-         let cardsRes = "";
+    let cardsRes = "";
 
-        beerDat.forEach((beerSort) => {
+    beerDat.forEach((beerSort) => {
 
         cardsRes += `<div class="cards">
 <img class="imag" src="${beerSort.image_url}">
 <p class="name">${beerSort.name}</p>
 <p class="description">${beerSort.description}</p>
-<p class="abv">${beerSort.abv}<span>% alc</span></p>
-<button onclick ="addBeerToBask(${beerSort.id})" class="btn__add"></button>
+<p class="abv">${beerSort.abv}<span>% alc</span></p>  
+${beerBasket.filter(item => beerSort.id === item.id).length ? `<button onclick ="deleteBeerFromBask(${beerSort.id})" class="btn__delete" ></button>` : `<button onclick ="addBeerToBask(${beerSort.id})" class="btn__add"></button>`}
 </div>`
-});
-cardsContainer.innerHTML = cardsRes;
+    });
+
+    cardsContainer.innerHTML = cardsRes;
 }
 
 function addBeerToBask(id) {
     beerBasket = [...beerDat.filter(card => card.id === id), ...beerBasket];
     localStorage.setItem('tovar', JSON.stringify(beerBasket));
+    cardRenders();
     updateCount(++countBeer);
-    console.log(countBeer)
-    cardBasketRenders();
 }
 
 function updateCount(value) {
@@ -104,10 +99,14 @@ function updateCount(value) {
 
 function deleteBeerFromBask(id) {
     beerBasket = beerBasket.filter(card => card.id !== id);
-    localStorage.setItem('tovar', JSON.stringify(beerBasket));
-    updateCount(--countBeer);;
     beerBasket = [...beerBasket];
+    localStorage.setItem('tovar', JSON.stringify(beerBasket));
+    updateCount(--countBeer);
+    //beerDat = beerDat.filter(card => card.id !== id);
+    //beerDat = [...beerDat];
+    cardRenders();
     cardBasketRenders()
+
 }
 document.querySelector('.bask__btn')
         .addEventListener('click', () => {
@@ -131,6 +130,7 @@ function showBasketModal (container) {
 
 }
 
+
 function cardBasketRenders () {
     let cardsRes = "";
 
@@ -146,4 +146,3 @@ function cardBasketRenders () {
     });
     cardBasketContainer.innerHTML = cardsRes;
 }
-
