2307f8abd1625e76c21027228a661f1999ccac2d
Anton <vasiliii.balda@gmail.com>
2022-07-10 22:19:41 +0300

    Here my 7th homework. Check please

---
+++ hw-7 NFT_Cards/hw-7.js
@@ -0,0 +1,5 @@
+const button = document.querySelector('.button');
+button.addEventListener('click', () => {
+    console.log('Ты сделал свою работу, моешь идти курить и какать')
+})
+console.log(2222)
