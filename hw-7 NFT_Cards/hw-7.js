af3a3b76a50e0e489ee2b19abb912c298fa13b2f
Anton <vasiliii.balda@gmail.com>
2022-07-13 15:37:30 +0300

    Here my 7th homework. Check please

--- hw-7 NFT_Cards/hw-7.js
+++ hw-7 NFT_Cards/hw-7.js
@@ -1,57 +1,57 @@
 const button = document.querySelector('.button');
 const cardField = document.querySelector('.wrapper');
 
-let card = [
-    {
-        firstField: 'Dima Maslenikov',
-        secondField: 'Name',
-        thirdField: 'card',
-        fourthField: 'description',
-        id: 1
+let card = []
+
+function getCard() {
+    return {
+        firstField: getRandomName(),
+        secondField: getRandomSurname(),
+        id: getRandomId()
     }
-]
+}
 
+function getRandomName() {
+    const name = ['Grisha', 'Pasha', 'Kolya', 'Vitya', 'Gena', 'Pavlik'];
+    return name [Math.floor(Math.random() * (name.length - 1)) + 1]
+}
 
-function addNewCard() {
-    cardField.innerHTML += `
-    <div class="wrapper-card">
+function getRandomSurname() {
+    const surname = ['Eldovoy', 'Podpechniy', 'Kukushkin', 'Clenoglotov', 'Mogilniy', 'Vodkin'];
+    return surname [Math.floor(Math.random() * (surname.length - 1)) + 1];
+
+}
+
+function getRandomId() {
+    return Math.floor(Math.random() * 1000)
+}
+
+function deleteCard(id) {
+    card = card.filter(el => el.id !== id);
+    cardRender()
+}
+
+function cardRender() {
+    let result = ''
+    for (let i = 0; i < card.length; i++) {
+        result +=
+            `<div class="wrapper-card">
     <div class="background-card">
-    <button class="move-element">
-    <img class="btn" src="Icons.png" alt="">
-    </button>
+    <div class="move-element">
+    <button onclick="deleteCard(${card[i]['id']})" class="btn"></button>
+    </div>
     <div class="card">
     <img class="img" src="https://random.imagecdn.app/500/500" alt="">
-    <p class="name">Card name</p>
-    <p class="description">card description</p>
+    <p class="name">${card[i]['firstField']}</p>
+    <p class="description">${card[i]['secondField']}</p>
     </div>
     </div>
 </div>`
+    }
+    cardField.innerHTML = result;
 }
 
 button.addEventListener('click', () => {
-    addNewCard()
+    card = [...card, getCard()];
+    cardRender()
 })
-
-
-// <div class="fake-border">
-//     <div class="background-card">
-//     <button class="move-element">
-//     <img class="btn" src="Icons.png" alt="">
-//     </button>
-//     <div class="card">
-//     </div>
-//     </div>
-//     </div>
-
-
-// <div class="card">
-//                     <div class="img">
-//                         <img class="image" src="https://images.unsplash.com/photo-1655365225170-55d6b7d2d6d5?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=500&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1NzQ3NDM5MA&ixlib=rb-1.2.1&q=80&w=500" alt="">
-//                         <div class="description">
-//                             <p class="deck-up up1">Card</p>
-//                             <p class="deck-up up2">Name</p>
-//                             <p class="deck-down down1">card</p>
-//                             <p class="deck-down down2">description</p>
-//                         </div>
-//                         </div>
-//                 </div>
